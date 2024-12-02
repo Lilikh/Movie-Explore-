6c8d55b36aa6c770fc87d4b21412428aa5d5a32f
Leila Kheirandish <81739933+Lilikh@users.noreply.github.com>
2024-12-02 22:09:06 +0100

    debug sth

--- src/api.js
+++ src/api.js
@@ -4,9 +4,8 @@ import axios from 'axios';
 const API_KEY = '65063cda';
 const BASE_URL = 'http://www.omdbapi.com/'; //http://www.omdbapi.com/?i=tt3896198&apikey=65063cda */
 
-const API_KEY = import.meta.env.VITE_API_KEY;
-const BASE_URL = import.meta.env.VITE_BASE_URL;
-
+const API_KEY = process.env.REACT_APP_API_KEY;
+const BASE_URL = process.env.REACT_APP_BASE_URL;
 
 export const searchMovies = async (query) => {
   try {

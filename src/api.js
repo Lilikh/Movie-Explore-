9ce14d028c196026d07fba06568ab35b33290828
Lilikh <81739933+Lilikh@users.noreply.github.com>
2025-04-13 20:46:03 +0200

    upadate the code

--- src/api.js
+++ src/api.js
@@ -1,28 +1,24 @@
 import axios from 'axios';
 
-
-const API_KEY = import.meta.env.API_KEY || process.env.API_KEY;
-const BASE_URL = 'http://www.omdbapi.com/'; //http://www.omdbapi.com/?i=tt3896198&apikey=65063cda
+const API_KEY = import.meta.env.VITE_API_KEY;
+const BASE_URL = import.meta.env.VITE_BASE_URL;
 
 export const searchMovies = async (query) => {
   try {
     const response = await axios.get(`${BASE_URL}?s=${query}&apikey=${API_KEY}`);
     return response.data;
   } catch (error) {
-    console.log(error);
+    console.error('Error fetching movies:', error);
     throw new Error('Failed to fetch movies');
   }
-  
 };
 
 export const getMovieDetails = async (id) => {
   try {
     const response = await axios.get(`${BASE_URL}?i=${id}&apikey=${API_KEY}`);
-    return response.data;   
-    
+    return response.data;
   } catch (error) {
-    console.log(error);
-    
+    console.error('Error fetching movie details:', error);
     throw new Error('Failed to fetch movie details');
   }
 };
